4dac815d3d1418377e0686900c9df98e78c8329b
vertigra <vertigra.developer@gmail.com>
2019-09-27 17:40:26 +1000

    Added ErrorDialogBox

---
+++ ExtendedDialogBox/PublicDialogBox/ErrorDialogBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace ExtendedDialogBox.PublicDialogBox
+{
+    class ErrorDialogBox : DialogBox
+    {
+        internal override MessageBoxImage DialogBoxImage => MessageBoxImage.Error;
+
+        public ErrorDialogBox(string message)
+        {
+            mDialogBox.Message = message;
+        }
+
+        public ErrorDialogBox(string message, string title)
+        {
+            mDialogBox.Message = message;
+            mDialogBox.Title = title;
+        }
+
+    }
+}
