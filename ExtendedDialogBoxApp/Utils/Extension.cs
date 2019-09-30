84d05523c476581d0e8969e35ed78ca0b2c3f5f6
vertigra <vertigra.developer@gmail.com>
2019-09-30 14:29:01 +1000

    Added extesion for tested secure string

---
+++ ExtendedDialogBoxApp/Utils/Extension.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ExtendedDialogBoxApp.Utils
+{
+    public static class Extension
+    {
+        public static string ToUnsecureString(this SecureString securePassword)
+        {
+            if (securePassword == null)
+            {
+                return string.Empty;
+            }
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        public static SecureString ToSecureString(this string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            var secureString = new SecureString();
+
+            foreach (var chr in password.ToCharArray())
+            {
+                secureString.AppendChar(chr);
+            }
+            secureString.MakeReadOnly();
+
+
+            return secureString;
+        }
+
+        public static bool SecureEqual(this SecureString secureString1, SecureString secureString2)
+        {
+            if (secureString1 == null)
+            {
+                throw new ArgumentNullException(nameof(secureString1));
+            }
+            if (secureString2 == null)
+            {
+                throw new ArgumentNullException(nameof(secureString2));
+            }
+
+            if (secureString1.Length != secureString2.Length)
+            {
+                return false;
+            }
+
+            IntPtr ss_bstr1_ptr = IntPtr.Zero;
+            IntPtr ss_bstr2_ptr = IntPtr.Zero;
+
+            try
+            {
+                ss_bstr1_ptr = Marshal.SecureStringToBSTR(secureString1);
+                ss_bstr2_ptr = Marshal.SecureStringToBSTR(secureString2);
+
+                string str1 = Marshal.PtrToStringBSTR(ss_bstr1_ptr);
+                string str2 = Marshal.PtrToStringBSTR(ss_bstr2_ptr);
+
+                return str1.Equals(str2);
+            }
+            finally
+            {
+                if (ss_bstr1_ptr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(ss_bstr1_ptr);
+                }
+
+                if (ss_bstr2_ptr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(ss_bstr2_ptr);
+                }
+            }
+        }
+    }
+}
