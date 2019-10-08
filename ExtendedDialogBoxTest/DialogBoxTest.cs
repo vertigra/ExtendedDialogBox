4f7c29b26ecf2472178e10203a4e05aa653bc7b2
vertigra <vertigra.developer@gmail.com>
2019-10-08 11:40:30 +1000

    Added test app.

---
+++ ExtendedDialogBoxTest/DialogBoxTest.cs
@@ -0,0 +1,15 @@
+﻿using ExtendedDialogBox.PublicDialogBox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtendedDialogBoxTest
+{
+    [TestClass]
+    public class DialogBoxTest
+    {
+        [TestMethod]
+        public void TestOkButton()
+        {
+            new DialogBox("TestMessage", "TestTitle").OkButton();
+        }
+    }
+}
